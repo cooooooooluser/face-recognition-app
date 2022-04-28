841c33cd59a4cd71f82126cd4dcec74f468f0327
cooooooooluser <85533967+cooooooooluser@users.noreply.github.com>
2022-04-27 18:49:56 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,107 @@
+prediction1="";
+prediction2="";
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : "jpeg",
+    jpeg_quality:101875023754
+});
+
+   camera= document.getElementById("Camera"); 
+   Webcam.attach( "#camera" );
+
+function capture() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = "<img id='captured' src="+ data_uri +">";
+    })
+}
+
+//https://teachablemachine.withgoogle.com/models/[...] 
+
+ImgClassifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/H5WodaQ8-/model.json", modelLoaded);
+
+function modelLoaded() {
+    console.log("modelLoaded");
+}
+
+function speak() {
+  var speak1 = window.speechSynthesis;
+  data1= "THE FIRST PREDICTION is " + prediction1;
+  data2= "THE  SECOND PREDICTION is " + prediction2;
+  utterThis = new SpeechSynthesisUtterance(data1, data2);
+  speak1.speak(utterThis);
+}
+
+function predict() {
+     img = document.getElementById("captured");
+     ImgClassifier.classify(img, gotresult);
+}
+
+function gotresult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+
+    else {
+        document.getElementById("Prediction-1").innerHTML = results[0].label;
+        document.getElementById("Prediction-2").innerHTML = results[1].label;
+        prediction1 = results[0].label;
+        prediction2 = results[1].label;
+        speak();
+
+        if (prediction1 == "happy") {
+            document.getElementById("Emoji-1").innerHTML = "&#128522";
+
+        }
+
+        if (prediction1 == "car") {
+            document.getElementById("Emoji-1").innerHTML = "U+1F697";
+
+        }
+
+        if (prediction1 == "nothing") {
+            document.getElementById("Emoji-1").innerHTML = "nothing";
+
+        }
+
+        if (prediction1 == "sad") {
+            document.getElementById("Emoji-1").innerHTML = "&#128532";
+             
+        }
+
+        if (prediction1 == "hardrive") {
+            document.getElementById("Emoji-1").innerHTML = "&#128436";
+             
+        }
+
+        if (prediction2 == "nothing") {
+            document.getElementById("Emoji-2").innerHTML = "nothing";
+             
+        }
+
+        if (prediction2 == "car") {
+            document.getElementById("Emoji-2").innerHTML = "U+1F697";
+             
+        }
+
+        if (prediction2 == "hardrive") {
+            document.getElementById("Emoji-2").innerHTML = "&#128436";
+             
+        }
+
+
+        if (prediction2 == "sad") {
+            document.getElementById("Emoji-2").innerHTML = "&#128532";
+             
+        }
+
+        if (prediction2 == "happy") {
+            document.getElementById("Emoji-2").innerHTML = "&#128522";
+             
+        }     
+
+        
+
+    }
+}
